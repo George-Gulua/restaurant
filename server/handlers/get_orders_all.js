fc53f6862c8ce19faa73c7e574d313de2a5dd5d2
traceattack <37976276+zero-numeral@users.noreply.github.com>
2022-12-03 20:35:35 +0300

    update

--- server/handlers/get_orders_all.js
+++ server/handlers/get_orders_all.js
@@ -3,7 +3,10 @@ const jwt = require("jsonwebtoken");
 const roles = require('../enum/roles');
 const { StatusCode } = require('status-code-enum');
 
+const User = require('../models/user');
+const Cart = require('../models/cart');
 const Order = require('../models/order');
+const Product = require("../models/product");
 
 module.exports = async function (req, res) {
     if (!req.query) {
@@ -23,7 +26,39 @@ module.exports = async function (req, res) {
                 return;
             }
 
-            res.status(StatusCode.SuccessOK).json(await Order.findAll());
+            let response = [];
+
+            const orders = await Order.findAll();
+            for (const order of orders) {
+                try {
+                    const carts = await Cart.findAll({where: {
+                        'OrderId': order.id,
+                    }});
+
+                    const user = await User.findByPk(order.UserId);
+                    
+                    entry = {};
+
+                    entry.order_id = order.id;
+                    entry.owner_login = user.login;
+                    entry.creation_date = order.creation_date;
+                    entry.products = [];
+
+                    for (const cart of carts) {
+                        const product = await Product.findByPk(cart.ProductId);
+                        entry.products.push(product);
+                    }
+
+                    response.push(entry);
+
+                } catch(err) {
+                    console.log(err);
+                    res.status(StatusCode.ServerErrorInternal).json();
+                    return;
+                }
+            }
+
+            res.status(StatusCode.SuccessOK).json(response);
             return;
         } catch (error) {
             console.log(`Wrong JWS token on get_orders_all: ${token}`);
